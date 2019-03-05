1f2df7db0408bc3a9d83b6d1fe9d701d079df2cb
ZKaiDi <zkdi@vip.qq.com>
2019-03-05 13:54:48 +0800

    优化
    添加表单提交验证，有空值时不能提交

--- pages/add/add.js
+++ pages/add/add.js
@@ -9,47 +9,54 @@ Page({
   },
 
   formSubmit(e) {
-    wx.showLoading({
-      title: '正在提交···',
-    })
-    const db = wx.cloud.database()
-    /*console.log('form发生了submit事件，携带数据为：', e.detail.value)*/
-    db.collection('work').add({
-      data:{
-        name:e.detail.value.name,
-        department:e.detail.value.department,
-        detaill:e.detail.value.detaill,
-        machine:this.data.mach,
-        date:e.detail.value.date
-      },
-        success:res=>{
+    //console.log(e.detail.value.name)
+    if(e.detail.value.name===''||e.detail.value.data===''||e.detail.value.department===''||e.detail.value.mach===''||e.detail.value.detaill===''){
+      wx.showModal({
+        title: '信息错误',
+        content: '请检查输入信息是否完整',
+      })
+    }else{
+      wx.showLoading({
+        title: '正在提交···',
+      })
+      const db = wx.cloud.database()
+      /*console.log('form发生了submit事件，携带数据为：', e.detail.value)*/
+      db.collection('work').add({
+        data: {
+          name: e.detail.value.name,
+          department: e.detail.value.department,
+          detaill: e.detail.value.detaill,
+          machine: this.data.mach,
+          date: e.detail.value.date
+        },
+        success: res => {
           wx.hideLoading()
           //返回结果处理
           wx.showToast({
-            title:"提交成功",
+            title: "提交成功",
           })
           /*console.log('提交成功: ',res._id)*/
         },
-        fail:err=>{
+        fail: err => {
           wx.hideLoading()
           wx.showToast({
-            icon:'none',
+            icon: 'none',
             title: '提交失败，请检查网络连接'
           })
           /*console.log('提交失败: ',err)*/
         }
       })
-    
-    db.collection('workadm').add({
-      data: {
-        name: e.detail.value.name,
-        department: e.detail.value.department,
-        detaill: e.detail.value.detaill,
-        machine: this.data.mach,
-        date: e.detail.value.date
-      },
-    })
-    
+
+      db.collection('workadm').add({
+        data: {
+          name: e.detail.value.name,
+          department: e.detail.value.department,
+          detaill: e.detail.value.detaill,
+          machine: this.data.mach,
+          date: e.detail.value.date
+        },
+      })
+    }
   },
 
   PickerChange(e) {
